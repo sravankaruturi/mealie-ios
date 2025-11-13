9c4b26bd24a3a5b80376c3ba53ee6d819289f474
Sravan Karuturi <Sravan@Sirishas-MacBook-Air.local>
2025-11-13 07:48:49 -0800

    Adding tests

--- mealieTests/mealieTests.swift
+++ mealieTests/mealieTests.swift
@@ -6,12 +6,9 @@
 //
 
 import Testing
-@testable import mealie
+// @testable import mealie
 
 struct mealieTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
 
 }
